111d4978037f3415b65a0a0e2410bf2f58226c90
jyr-king <jyr.king@gmail.com>
2018-03-14 11:04:07 +0800

    updates for guidata and guiqwt

---
+++ Examples/VNA_setup_guidata.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 12 22:50:13 2018
+
+@author: jyr_king
+"""
+
+import numpy as np
+from PyQCLab.Instrument.NetworkAnalyser import *
+from PyQCLab.Instrument.instr_config import *
+import guidata.qt.QtCore as QtCore
+import guidata.qt.QtGui as QtGui
+
+import guidata.dataset.datatypes as gdt
+import guidata.dataset.dataitems as gdi
+import guidata.dataset.qtwidgets as gdq
+import guidata.configtools as configtools
+
+import guiqwt.plot as gqp
+import guiqwt.curve as gqc
+import guiqwt.image as gqi
+import guiqwt.tools as gqt
+import time
+
+class Vna_Parameters(gdt.DataSet):
+    g1 = gdt.BeginGroup('Sweep setups:')
+    swp_mode=gdi.ChoiceItem('Sweep Mode:',[('hold','Hold'),('cont','Continuous'),('gro','Groups'),('sing','Single')])
+    swp_type=gdi.ChoiceItem('Sweep Type:',[('lin','Linear'),('log','Logarithmic'),('pow','Power'),('cw','CW'),\
+                                           ('segm','Segment'),('phas','Phase')])
+    swp_points=gdi.IntItem('Sweep Points:',min=ZNB20_CONSTS['POINTMIN'],max=ZNB20_CONSTS['POINTMAX'],default=10001)
+    swp_IFbandwidth=gdi.IntItem('IF_Bandwidth:',min=ZNB20_CONSTS['IFMIN'],max=ZNB20_CONSTS['IFMAX'],default=1000)
+    swp_average=gdi.IntItem('Average:',min=1,default=1)
+    _g1=gdt.EndGroup('Sweep setups:')
+    
+    _prop = gdt.GetAttrProp("choice")
+    g2 = gdt.BeginGroup('Frequency Setup:')
+    choice = gdi.ChoiceItem('',[('SS','Start Stop'),('CS','Center Span')],radio=False)\
+                            .set_prop('display',store=_prop)
+    fstart = gdi.FloatItem('Start (GHz):',min=ZNB20_CONSTS['FREQMIN']/1e9,\
+                           max=ZNB20_CONSTS['FREQMAX']/1e9,default=4)\
+                           .set_prop('display',active=gdt.FuncProp(_prop, lambda x: x=='SS'))
+    fstop = gdi.FloatItem('Stop (GHz):',min=ZNB20_CONSTS['FREQMIN']/1e9,\
+                          max=ZNB20_CONSTS['FREQMAX']/1e9,default=8)\
+                          .set_prop('display',active=gdt.FuncProp(_prop, lambda x: x=='SS'))
+    fcenter = gdi.FloatItem('Center (GHz):',min=ZNB20_CONSTS['FREQMIN']/1e9,\
+                            max=ZNB20_CONSTS['FREQMAX']/1e9,default=8)\
+                            .set_prop('display',active=gdt.FuncProp(_prop, lambda x: x=='CS'))
+    fspan = gdi.FloatItem('Span (GHz):',min=ZNB20_CONSTS['FREQMIN']/1e9,\
+                          max=ZNB20_CONSTS['FREQMAX']/1e9,default=8)\
+                          .set_prop('display',active=gdt.FuncProp(_prop, lambda x: x=='CS'))
+    _g2 = gdt.EndGroup('Frequency Setup:')
+    
+    g3 = gdt.BeginGroup('Power Setup:')
+    pwr = gdi.FloatItem('Power (dBm):',min=ZNB20_CONSTS['POWMIN'],max=ZNB20_CONSTS['POWMAX'],default=-12)
+    _g3 = gdt.EndGroup('Power Setup:')
+    
+    g4 = gdt.BeginGroup('Save:')
+    save_enable = gdi.BoolItem("Enable Data Save",
+                      help="If disabled, the following parameters will be ignored",
+                      default=False)
+    path = gdi.DirectoryItem('Directory:','C:/Users/jyr_king/Documents/')
+    filename = gdi.StringItem('File Name:',time.strftime("%Y-%m-%d", time.localtime())+'_VNA_sweep')
+    _g4 = gdt.EndGroup('Save:')
+    
+#Vna_Parameters.active_setup()    
+    
+if __name__ == "__main__":
+    # -- Create QApplication
+    import guidata
+    _app = guidata.qapplication()
+    
+    vna = Vna_Parameters()
+    if vna.edit():
+        print(vna)
+    vna.view()
+#    stamp_gbox = gdq.DataSetEditGroupBox("Dots", Vna_Parameters)
+#    stamp_gbox.show()
+    x=vna.swp_IFbandwidth
+    print(vna.choice,x)
--- README.md
+++ README.md
@@ -1,3 +1,58 @@
 # PyQCLab
 
-PyQCLab is aimed to help control instruments and time sequences for superconducting quantum computing. 
+# PyQCLab实验控制测量软件架构
+> 主要开发者：金怡荣，王战
+## 简介
+PyQCLab是针对超导量子计算实验研究开发的简单仪器控制与测量、数据处理和可视化相关的应用程序。目前主要开发语言是Python，不排除今后引入其他开发语言进行优化。
+
+## 运行环境与安装
+
+### PyQCLab是与平台无关的可移植开源软件。
+依赖的运行环境和库如下：
+- Python 3.5+
+- Numpy and Scipy
+- matplotlib
+- pyvisa
+- guidata, guiqwt
+- scikit-rf
+- h5py
+- ...
+
+### 安装：
+推荐使用[Anaconda](https://www.anaconda.com/download/)集成python科学运算库. Anaconda集成了几乎最常见的Python开发环境和科学运算库，包括numpy, scipy, matplotlib, ipython, notebook, spyder等等，并且提供良好的包管理（conda）系统。
+
+以下以Anaconda安装为例，介绍在windows下安装运行环境：
+1. 下载Anaconda for Windows；注意选择 ==Python 3 Version==
+2. 运行安装文件，所有的步骤采用推荐设置即可；
+3. 安装完成之后，运行"Anaconda prompt"，系统将弹出一个终端
+4. 安装必要的库：
+
+```
+> conda install scikit-rf
+> pip install pyvisa
+> pip install guidata
+> pip install guiqwt
+```
+> 注意：guiqwt是pythonqwt的封装，而pythonqwt是Qt绘图库qwt的封装，qwt绘图库安装需要编译，会调用C++编译器，如果系统中没有C++编译器，则会出现安装错误。从Windows官网上下载Visual C++ Tools，安装即可，版本要求14.0以上。
+
+### 文件组织
+PyQCLab 
+```
+graph TD
+PyQCLab-->Instrument
+PyQCLab-->APPs
+PyQCLab-->Utils
+PyQCLab-->UIs
+PyQCLab-->Documents
+
+
+```
+- Instrument: 包含各仪器设备驱动的python封装；
+- Apps：用于实际进行控制和测量的脚本或可执行文件；
+- Utils: 数据处理和生成相关的模块；
+- UIs：界面相关的模块；
+- Documents：仪器设备、各python库的相关文档；
+
+    
+
+
